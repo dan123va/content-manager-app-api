6e314ad4207cc36c5c8582aaba17fe889c3a41b5
Daniel Silverio <daniel.silverio@mariachi.io>
2022-08-12 16:47:35 -0500

    Se agrego el codigo para agregar, obtener y modificar un recurso de la pagina web

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const path = require("path");
 const pathToFile = path.resolve("./data.json");
 
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
@@ -14,6 +17,47 @@ app.get("/", (req, res) => {
 app.get("/api/resources", (req, res) => {
   const resources = getResources();
   res.send(resources);
+});
+
+app.post("/api/resources", (req, res) => {
+  const resources = getResources();
+  const resource = req.body;
+
+  resource.createAt = new Date();
+  resource.status = "inactive";
+  resource.id = Date.now().toString();
+  resources.unshift(resource);
+
+  fs.writeFileSync(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+  });
+
+  return res.send("Data has been saved!");
+});
+
+app.get("/api/resources/:id", (req, res) => {
+  const resources = getResources();
+  const { id } = req.params;
+  const resource = resources.find((resource) => resource.id === id);
+
+  res.send(resource);
+});
+
+app.patch("/api/resources/:id", (req, res) => {
+  const resources = getResources();
+  const { id } = req.params;
+  const index = resources.findIndex((resource) => resource.id === id);
+  resources[index] = req.body;
+
+  fs.writeFileSync(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+  });
+
+  res.send("Data has been updated!");
 });
 
 app.listen(PORT, () => {
